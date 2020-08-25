0c309fa4aa52bafecd3ba77e3d9efc86342d50b5
Harper <niufei12z8888@gmail.com>
2020-08-25 16:19:38 +0800

    optimized matrix show logic.
    add cascaded led matrix support.
    
    Signed-off-by: Harper <niufei12z8888@gmail.com>

--- bsp/stm32/stm32f407-atk-explorer/applications/matrix_show.c
+++ bsp/stm32/stm32f407-atk-explorer/applications/matrix_show.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include <rthw.h>
 #include <rtdevice.h>
 #include <board.h>
@@ -14,7 +16,10 @@
 static struct rt_thread matrix_thread;
 ALIGN(RT_ALIGN_SIZE)
 static char matrix_thread_stack[MATRIX_THREAD_STACK_SIZE];
+uint8_t matrix_out_data[MATRIX_MAX_CASCADE_NUM * 2 * 32];
 #endif
+
+uint8_t *matrix_out;
 
 /*
 	A------>PA0
@@ -83,6 +88,7 @@ static char matrix_thread_stack[MATRIX_THREAD_STACK_SIZE];
 
 static void matrix_io_init(void)
 {
+	//set all port as output mode.
 	rt_pin_mode(MATRIX_ADDR_A, PIN_MODE_OUTPUT);
 	rt_pin_mode(MATRIX_ADDR_B, PIN_MODE_OUTPUT);
 	
@@ -100,6 +106,7 @@ static void matrix_io_init(void)
 
 static void matrix_set_line(uint8_t line)
 {
+	//two address port include 0b'00 01 10 11.
 	switch(line)
 	{
 		case 0:
@@ -127,45 +134,32 @@ static void matrix_set_line(uint8_t line)
 
 static void matrix_show( void )
 {
+	uint8_t dat_low;
+	uint8_t dat_low1;
+	
+	uint8_t dat_high;
+	uint8_t dat_high1;
+	
 	//行扫描
-	uint8_t dat1;
-	uint8_t dat2;
-	uint16_t data1=0;
-	uint16_t data2=0;
 	for(int n = 0; n<4; n++)
 	{
 		//流转递进
 		matrix_set_line((uint8_t)(n));
-		for( int i = 0; i< 4;i++)
+		for( int i = 0; i< 2*2*MATRIX_CASCADE_NUM; i++)
 		{
-			
 			MATRIX_OE_H;
 			MATRIX_LAT_L;
 			
-			if( i < 2)
-			{
-				dat1 = word_date[0][n*2+i];
-				dat2 = word_date[0][2*n+8+i];
-				data1 = dat1 | (dat2<<8);
-				
-				dat1 = word_date[0][n*2+16+i];
-				dat2 = word_date[0][2*n+24+i];
-				data2 = dat1 | (dat2<<8);
-			}
-			else
-			{
-				dat1 = word_date[1][n*2+i-2];
-				dat2 = word_date[1][2*n+8+i-2];
-				data1 = dat1 | (dat2<<8);
-				
-				dat1 = word_date[1][n*2+16+i-2];
-				dat2 = word_date[1][2*n+24+i-2];
-				data2 = dat1 | (dat2<<8);
-			}
+			dat_high = matrix_out[(i/2)*32+(i%2) + n*2];
+			dat_high1 = matrix_out[(i/2)*32+(i%2)+ n*2+8];
+			
+			dat_low = matrix_out[(i/2)*32+(i%2)+ n*2+16];
+			dat_low1 = matrix_out[(i/2)*32+(i%2)+ n*2+24];
+			
 			for( int k = 0; k < 8; k++)
 			{
 				MATRIX_CLK_L;
-				if( data1 & (1<<(15-k)))
+				if( dat_high1 & (1<<(7-k)))
 				{
 					MATRIX_R1_H;
 				}
@@ -173,7 +167,8 @@ static void matrix_show( void )
 				{
 					MATRIX_R1_L;
 				}
-				if( data2 & (1<<(15-k)))
+				
+				if( dat_low1 & (1<<(7-k)))
 				{
 					MATRIX_R2_H;
 				}
@@ -188,7 +183,7 @@ static void matrix_show( void )
 		{
 
 				MATRIX_CLK_L;
-				if( data1 & (1<<k))
+				if( dat_high & (1<<k))
 				{
 					MATRIX_R1_H;
 				}
@@ -196,7 +191,7 @@ static void matrix_show( void )
 				{
 					MATRIX_R1_L;
 				}
-				if( data2 & (1<<k))
+				if( dat_low & (1<<k))
 				{
 					MATRIX_R2_H;
 				}
@@ -216,9 +211,21 @@ static void matrix_show( void )
 	
 }
 
+static void matrix_buffer_prepare(void)
+{
+	for(int i =0; i < MATRIX_MAX_CASCADE_NUM * 2; i++)
+	{
+		memcpy(&matrix_out[i*32],word_date[i] , 32);
+	}
+}
+
 static void matrix_thread_entry(void *parameter)
 {
 	matrix_io_init();
+	if(matrix_out == NULL)
+		rt_kprintf("matrix out buffer error\n");
+
+	matrix_buffer_prepare();
 	while(1)
 	{
 		//rt_kprintf("matrix show\n");
@@ -233,10 +240,14 @@ int matrix_thread_init(void)
     rt_thread_t tid;
 	
 #ifdef RT_USING_HEAP
+	matrix_out = (uint8_t *)rt_calloc(1, sizeof(uint8_t) * MATRIX_MAX_CASCADE_NUM * 2 * 32);
+	memset(matrix_out, 0, sizeof(uint8_t) * MATRIX_MAX_CASCADE_NUM * 2 * 32);
+	
 	tid = rt_thread_create(MATRIX_THREAD_NAME,
 						   matrix_thread_entry, RT_NULL,
 						   MATRIX_THREAD_STACK_SIZE, MATRIX_THREAD_PRIORITY, 10);
 #else
+	matrix_out = matrix_out_data;
 	result = rt_thread_init(&matrix_thread,
 							MATRIX_THREAD_NAME,
 							matrix_thread_entry, RT_NULL,
--- bsp/stm32/stm32f407-atk-explorer/applications/matrix_show.h
+++ bsp/stm32/stm32f407-atk-explorer/applications/matrix_show.h
@@ -3,11 +3,12 @@
 
 #include <rtthread.h>
 
-#define MATRIX_STEP_NUM				1
+#define MATRIX_CASCADE_NUM					1
+#define MATRIX_MAX_CASCADE_NUM				10
 
 #define MATRIX_THREAD_NAME	"matrix"
 #define MATRIX_THREAD_STACK_SIZE	2048
-#define MATRIX_THREAD_PRIORITY		10
+#define MATRIX_THREAD_PRIORITY		21
 
 
 #endif
